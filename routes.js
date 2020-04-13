d6b8daac5312035a8473458df5f20affc6f6d2e6
Kevin Naranjo <knaranjo357@unab.edu.co>
2020-04-13 11:21:09 -0500

    desktop and routes

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path:'/'
+        template: <h1>hola</h1>
+    },
+    {
+        path: '/contacto'
+        template: <h1>contacto</h1>
+    },
+    {
+        path: '/'
+        template: <h1>hola</h1>
+    }
+]
